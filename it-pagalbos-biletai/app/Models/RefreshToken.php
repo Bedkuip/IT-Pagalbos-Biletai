6b8eda4cc0841e1aa4c6f848490010640c4ac5db
Bedkuip <160793735+Bedkuip@users.noreply.github.com>
2025-12-02 18:20:27 +0200

    Login and Refresh Functional

--- it-pagalbos-biletai/app/Models/RefreshToken.php
+++ it-pagalbos-biletai/app/Models/RefreshToken.php
@@ -7,7 +7,12 @@ class RefreshToken extends Model {
     protected $fillable = [
         'user_id','token_hash','expires_at','revoked_at','user_agent','ip'
     ];
-    protected $dates = ['expires_at','revoked_at'];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'revoked_at' => 'datetime',
+    ];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
